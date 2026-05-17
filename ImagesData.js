0b51e2787eced6daab8b096889f893863eb8d807
catalina <catalinaruedaf@hotmail.com>
2026-05-17 15:16:30 +1000

    doign the styles

---
+++ ImagesData.js
@@ -0,0 +1,24 @@
+
+import imagesproduct1 from "./src/assets/images/image-product-1.jpg";
+import imagesproduct2 from "./src/assets/images/image-product-2.jpg";
+import imagesproduct3 from "./src/assets/images/image-product-3.jpg";
+import imagesproduct4 from "./src/assets/images/image-product-4.jpg";
+import thumbnailproduct1 from "./src/assets/images/image-product-1-thumbnail.jpg";
+import thumbnailproduct2 from "./src/assets/images/image-product-2-thumbnail.jpg";
+import thumbnailproduct3 from "./src/assets/images/image-product-3-thumbnail.jpg";
+import thumbnailproduct4 from "./src/assets/images/image-product-4-thumbnail.jpg";
+
+
+export const images = {
+    imagesproduct1,
+    imagesproduct2,
+    imagesproduct3,
+    imagesproduct4
+};
+
+export const thumbnails = {
+    thumbnailproduct1,
+    thumbnailproduct2,
+    thumbnailproduct3,
+    thumbnailproduct4
+};
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,93 @@
+
+import logo from '../assets/images/logo.svg';
+import shoppingCart from '../assets/images/icon-cart.svg';
+import avatar from '../assets/images/image-avatar.png';
+import hamburger from '../assets/images/icon-menu.svg';
+import closeIcon from '../assets/images/icon-close.svg';
+
+import { useState } from "react";
+
+
+function Navigation() {
+   
+
+const[menuOpen,setMenuOpen]= useState(false);
+ // close the menu when clicking outside
+
+  function handleLinkClick(){
+    setMenuOpen(false);
+  }
+
+    return (
+<section className="navigation-section overlay active">
+
+<header className="header ">
+    <div className="header__inner">
+               
+
+            {/* hamburguer button only visible on mobile */}
+            
+
+            <button
+                className="menu-btn"
+                aria-label="toggle menu"
+                onClick={() => setMenuOpen(!menuOpen)}
+            >
+            {menuOpen ? <img src={closeIcon} alt="Close Icon" /> : <img src={hamburger} alt="Hamburger Menu Icon" />}
+            </button>
+
+            {/* Nav list, toggled open on mobile */}
+             <a href="/" className="nav-logo">
+                <img src={logo} className=" nav-logo" alt="a pair of sneakers shoes" />
+                </a>
+
+        <nav className="navigation" aria-label="homepage Navigation">
+                
+                <ul className={`nav__list ${menuOpen ? "open" : ""}`}>
+                    {/* <li className="nav-logo">
+                    
+                        <img src={logo} className="mobile-logo nav-logo" alt="a pair of sneakers shoes" />
+                    
+                </li> */}
+                    <li className="nav__items">
+
+                        <a href="/Collections" 
+                        onClick={handleLinkClick}
+                        className="nav__items__links">Collections</a>
+                    </li>
+                    <li className="nav__items">
+                        <a href="/team" 
+                        onClick={handleLinkClick}
+                        className="nav__items__links">Men</a>
+                    </li>
+                    <li className="nav__items">
+                        <a href="/sign-in" 
+                        onClick={handleLinkClick}
+                        className="nav__items__links">Women</a>
+                    </li>
+                    <li className="nav__items">
+                        <a href="/about" 
+                        onClick={handleLinkClick}
+                        className="nav__items__links">About</a>
+                    </li>
+                    <li className="nav__items">
+                        <a href="/contact" 
+                        onClick={handleLinkClick}
+                        className="nav__items__links">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+    </div>
+    <div className="shopping-cart">
+        <img className="shopping-cart-icon" src={shoppingCart} alt="shopping cart icon" />
+        <img src={avatar} alt="avatar icon" className="avatar" />
+    </div>
+ </header>
+
+</section>
+    )
+}
+
+
+
+export default Navigation;
